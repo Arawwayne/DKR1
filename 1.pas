635439b796b3f8ce0f992b782c3dd08458676f81
Wayne <69819453+Arawwayne@users.noreply.github.com>
2024-10-17 19:17:32 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,22 @@
+﻿{1.	Написать программу, вычисляющую значение функции:
+    x ^ 2, если x < -8;
+    lg(x) / cos(x) + cos(x), если -8 <= x < -3;
+    lg(x) * e ^ x, если -3 <= x.                      }
+
+var
+  x: real;
+
+begin
+  Readln(x);
+  if x < -8 then Writeln(x**2)
+  else if x < 3 then
+  begin
+    if x > 0 then Writeln(Log10(x) / Cos(x) + Cos(x))
+    else Writeln('Отрицательного логарифма не существует!');
+  end
+  else
+  begin
+    if x > 0 then Write(Log10(x) * E**x)
+    else Writeln('Отрицательного логарифма не существует!');
+  end;
+end.
---
+++ 2.pas
@@ -0,0 +1,31 @@
+﻿//2. Вычислить значение функции на интервале [-10; -1] с шагом 0,1.}
+
+var
+  a, b: integer;
+  s: real;
+
+function f(x: real): real;
+begin
+  if x < -8 then Write(x**2, ' ')
+  else if x < 3 then
+  begin
+    if x > 0 then Write(Log10(x) / Cos(x) + Cos(x), ' ')
+    else Write('Отрицательного логарифма не существует! ');
+  end
+  else
+  begin
+    if x > 0 then Write(Log10(x) * E**x, ' ')
+    else Write('Отрицательного логарифма не существует! ');
+  end;
+end;
+
+begin
+  a := -10;
+  b := -1;
+  s := a;
+  while s < b do 
+  begin
+    f(s);
+    s += 0.1;
+  end;
+end.
